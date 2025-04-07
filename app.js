06dfeaa4bd9377b8546b200a588d123a71bf2142
Your Name <you@example.com>
2025-04-07 13:26:34 +0300

    bu benim ilk projem

---
+++ app.js
@@ -0,0 +1,5 @@
+let ad = "Ahmet";
+let soyad = "Öner";
+let yas = 20;
+let dogumTarihi = 2005;
+window.alert("Merhaba " + ad + " " + soyad + " " + yas + " yaşındasın ve " + dogumTarihi + " doğumlusun.");
